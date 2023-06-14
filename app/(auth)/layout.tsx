7b51cc0bfd89e31b9973c2dc31bc5a44d29fbd3c
OmarMWarraich <earthling986@gmail.com>
2023-06-14 18:14:57 +0500

    feat: Layouts

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import "../../styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin-ext"],
+});
+
+import GlassPane from "@/components/GlassPane";
+
+export default function AuthRootLayout({children}) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane
+          className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  )
+}
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,21 @@
+import GlassPane from "@/components/GlassPane";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin-ext"],
+});
+
+export default function DashboardRootLayout({children}) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane
+          className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  )
+}
